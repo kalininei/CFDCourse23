1edd45c382c6671ffb161f378502df5585153a65
ek <kalininei@yandex.ru>
2023-10-28 11:30:54 +0300

    jacobi solver for triplet matrix

--- progs/sandbox.cpp
+++ progs/sandbox.cpp
@@ -15,6 +15,71 @@ struct ISparseMatrix{
 	virtual std::vector<double> diagonal() const = 0;
 };
 
+struct TripletSparseMatrix: public ISparseMatrix{
+	TripletSparseMatrix(size_t n_rows): _n_rows(n_rows){}
+	size_t n_rows() const override{
+		return _n_rows;
+	};
+	void set_value(size_t irow, size_t icol, double value) override{
+		int addr = find_by_row_col(irow, icol);
+		if (addr >= 0){
+			_vals[addr] = value;
+		} else {
+			_rows.push_back(irow);
+			_cols.push_back(icol);
+			_vals.push_back(value);
+		}
+	}
+	std::vector<double> mult(
+			const std::vector<double>& x) const override{
+		std::vector<double> ret(n_rows(), 0);
+		for (size_t irow=0; irow < n_rows(); ++irow){
+				ret[irow] = mult_row(irow, x);
+		}
+		return ret;
+	}
+	double mult_row(size_t irow,
+			const std::vector<double>& x) const override{
+		double sum = 0;
+		for (size_t icol=0; icol < n_rows(); ++icol){
+				sum += value(irow, icol) * x[icol];
+		}
+		return sum;
+	}
+	std::vector<double> diagonal() const override{
+		std::vector<double> ret(n_rows());
+		for (size_t i=0; i<n_rows(); ++i){
+			ret[i] = value(i, i);
+		}
+		return ret;
+	}
+private:
+	const size_t _n_rows;
+	std::vector<int> _rows;
+	std::vector<int> _cols;
+	std::vector<double> _vals;
+
+	double value(size_t irow, size_t icol) const{
+		int a = find_by_row_col(irow, icol);
+		if (a >= 0){
+			return _vals[a];
+		} else {
+			return 0.0;
+		}
+	}
+
+	// returns >= 0 if [irow, icol] found, else -1
+	int find_by_row_col(size_t irow, size_t icol) const{
+		for (size_t a=0; a<_rows.size(); ++a){
+			if (_rows[a] == (int)irow
+			    && _cols[a] == (int)icol){
+				return (int)a;
+			}
+		}
+		return -1;
+	}
+};
+
 struct ISolver{
 	virtual ~ISolver() = default;
 	ISolver(const ISparseMatrix* mat): _mat(mat){}
@@ -24,21 +89,26 @@ protected:
 	const ISparseMatrix* _mat;
 };
 
-struct JacobiSolver: public ISolver{
-	JacobiSolver(const ISparseMatrix* mat, size_t maxit, double eps):
+struct IIterativeSolver: public ISolver{
+	virtual ~IIterativeSolver() = default;
+	IIterativeSolver(const ISparseMatrix* mat, size_t maxit, double eps):
 		ISolver(mat), _maxit(maxit), _eps(eps){}
 
-	void solve(const std::vector<double>& rhs, std::vector<double>& x) override{
-		std::vector<double> diag = _mat->diagonal();
-		for (size_t it=0; it<_maxit; ++it){
-			// iterations
-			std::vector<double> x_new(x);
-			for (size_t irow=0; irow<_mat->n_rows(); ++irow){
-				x_new[irow] += (rhs[irow] - _mat->mult_row(irow, x))/diag[irow];
-			}
+	void set_saver(std::shared_ptr<ASpatialApproximator> appr, const std::string& fname){
+		_appr = appr;
+		_writer = std::make_shared<TimeDependentWriter>(fname);
+	}
 
-			// to the next layer: x = x_new
-			std::swap(x, x_new);
+	void solve(const std::vector<double>& rhs, std::vector<double>& x) override{
+		for (size_t it=0; it<_maxit; ++it){
+			// iteration step
+			iteration_step(rhs, x);
+
+			// save solution
+			if (_appr){
+				std::string fname = _writer->add(it);
+				_appr->vtk_save_scalars(fname, {{"data", &x}});
+			}
 
 			// residual
 			std::vector<double> lhs = _mat->mult(x);
@@ -48,6 +118,7 @@ struct JacobiSolver: public ISolver{
 				norm_max = std::max(norm_max, res);
 			}
 			if (norm_max < _eps){
+				std::cout << "converged in " << it << " iterations" << std::endl;
 				break;
 			}
 		}
@@ -55,30 +126,39 @@ struct JacobiSolver: public ISolver{
 private:
 	const size_t _maxit;
 	const double _eps;
+	std::shared_ptr<ASpatialApproximator> _appr;
+	std::shared_ptr<TimeDependentWriter> _writer;
+
+	virtual void iteration_step(const std::vector<double>& rhs, std::vector<double>& x) = 0;
+};
+
+struct JacobiSolver: public IIterativeSolver{
+	JacobiSolver(const ISparseMatrix* mat, size_t maxit, double eps):
+		IIterativeSolver(mat, maxit, eps){}
+
+private:
+	void iteration_step(const std::vector<double>& rhs, std::vector<double>& x) override{
+		std::vector<double> diag = _mat->diagonal();
+		std::vector<double> x_new(x);
+		for (size_t irow=0; irow<_mat->n_rows(); ++irow){
+			x_new[irow] += (rhs[irow] - _mat->mult_row(irow, x))/diag[irow];
+		}
+		// to the next layer: x = x_new
+		std::swap(x, x_new);
+	}
 };
 
 
 void test(){
-	ISparseMatrix* mat;
 	size_t N = 10;
 	double A = 1;
 	double B = 2;
 	double h = 1.0/(N-1);
 
+	ISparseMatrix* mat = new TripletSparseMatrix(N);
 	std::shared_ptr<ARegularGrid> grid = RegularGrid1::build(N, 1);
 	std::shared_ptr<FdmApproximator> appr = FdmApproximator::build(grid);
 
-
-	std::vector<double> data(N, 0);
-	TimeDependentWriter writer("jacobi");
-	{
-		std::string fname = writer.add(0);
-		appr->vtk_save_scalars(fname, {{"data", &data}});
-	}
-	{
-		std::string fname = writer.add(1);
-		appr->vtk_save_scalars(fname, {{"data", &data}});
-	}
 
 	// ==== Fill matrix
 	// 1)
@@ -105,7 +185,8 @@ void test(){
 
 	// ================== Solution
 	std::vector<double> u(N, 0);
-	ISolver* solver = new JacobiSolver(mat, 10'000, 1e-3);
+	IIterativeSolver* solver = new JacobiSolver(mat, 10'000, 1e-3);
+	solver->set_saver(appr, "jacobi");
 
 	solver->solve(f, u);
 
